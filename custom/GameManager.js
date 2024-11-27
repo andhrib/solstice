48a8db4c41bc095a328f958b58a92cac180d6475
maj25 <ma2144@student.uni-lj.si>
2024-11-27 19:02:15 +0100

    UI

--- custom/GameManager.js
+++ custom/GameManager.js
@@ -1,84 +1,128 @@
-import { Generator } from './Generator.js';
-import { GLTFLoader } from '../engine/loaders/GLTFLoader.js';
+import { Generator } from "./Generator.js";
+import { GLTFLoader } from "../engine/loaders/GLTFLoader.js";
 
-import { gridCenter } from './gameParameters.js';
+import { gridCenter } from "./gameParameters.js";
 
 export class GameManager {
-    constructor() {
-        this.generators = [];
-        this.loader = new GLTFLoader();
-        this.idx = 0;
-        this.counts = [3, 2, 1];
-        this.n = this.counts.length;
-        this.typesLeft = this.n;
-        this.points = 0;
-        this.target = 13;
+  constructor() {
+    this.generators = [];
+    this.loader = new GLTFLoader();
+    this.idx = 0;
+    this.counts = [3, 2, 1];
+    this.n = this.counts.length;
+    this.typesLeft = this.n;
+    this.points = 0;
+    this.target = 13;
+  }
+
+  async initialize(scene, tileArr) {
+    await this.loader.load("/models/generator/Generators_pack_V2.gltf");
+    const generatorScene = this.loader.loadScene(this.loader.defaultScene);
+
+    for (let i = 0; i < this.n; i++) {
+      const generatorNode = generatorScene.children[i];
+      generatorNode.enabled = false;
+      const generator = new Generator(scene, generatorNode, tileArr, i, this);
+      generatorNode.addComponent(generator);
+      this.generators.push(generator);
     }
-    
-    async initialize(scene, tileArr) {
-        await this.loader.load("/models/generator/Generators_pack_V2.gltf");
-        const generatorScene = this.loader.loadScene(this.loader.defaultScene);
-        
-        for (let i = 0; i < this.n; i++) {
-            const generatorNode = generatorScene.children[i];
-            generatorNode.enabled = false;
-            const generator = new Generator(scene, generatorNode, tileArr, i, this)
-            generatorNode.addComponent(generator);
-            this.generators.push(generator);
-        }
-        for (let i = 0; i < this.n; i++) {
-            this.generators[i].changeParent(scene);
-        }
-        
-        this.generators[this.idx].enable();
-        window.addEventListener("keydown", this.handleKeyDown.bind(this));
-        this.points += tileArr[gridCenter][gridCenter].setPermaStatus("yellow");
+    for (let i = 0; i < this.n; i++) {
+      this.generators[i].changeParent(scene);
     }
 
-    handleKeyDown(event) {
-        if (this.typesLeft == 0) { 
-            return;
-        }
-        if (event.key === "q") {
-            this.generators[this.idx].disable();
-            this.idx = (this.idx + 1) % this.n;
-            while (this.counts[this.idx] == 0) {
-                this.idx = (this.idx + 1) % this.n;
-            }
-            while (!this.generators[this.idx].enable()) {
-                this.generators.splice(this.idx, 1);
-            }
-        }
-        if (event.key === "e") {
-            this.generators[this.idx].disable();
-            this.idx = (this.idx - 1 + this.n) % this.n;
-            while (this.counts[this.idx] == 0) {
-                this.idx = (this.idx - 1 + this.n) % this.n;
-            }
-            this.generators[this.idx].enable();
-        }
+    this.generators[this.idx].enable();
+    window.addEventListener("keydown", this.handleKeyDown.bind(this));
+    this.points += tileArr[gridCenter][gridCenter].setPermaStatus("yellow");
+
+    //UI
+    this.element = document.getElementById("b1");
+    if (this.element) {
+      this.element.addEventListener("click", () => this.selectBuilding(0));
+    }
+    this.element = document.getElementById("b2");
+    if (this.element) {
+      this.element.addEventListener("click", () => this.selectBuilding(1));
+    }
+    this.element = document.getElementById("b3");
+    if (this.element) {
+      this.element.addEventListener("click", () => this.selectBuilding(2));
     }
 
-    decrementCount() {
-        this.counts[this.idx]--;
-        //console.log(this.counts[this.idx]);
-        if (this.counts[this.idx] == 0) {
-            this.typesLeft--;
-            this.generators[this.idx].disable();
-            if (this.typesLeft == 0) {
-                if (this.points >= this.target) {
-                    console.log("You win!");
-                }
-                else {
-                    console.log("You lose.");
-                }
-                return;
-            }
-            while (this.counts[this.idx] == 0) {
-                this.idx = (this.idx + 1) % this.n;
-            }
-            this.generators[this.idx].enable();
+    this.updateUI();
+  }
+
+  selectBuilding(id) {
+    if (this.counts[id] > 0) {
+      this.generators[this.idx].disable();
+      this.idx = id;
+      this.generators[this.idx].enable();
+    }
+  }
+
+  handleKeyDown(event) {
+    if (this.typesLeft == 0) {
+      return;
+    }
+    if (event.key === "q") {
+      this.generators[this.idx].disable();
+      this.idx = (this.idx + 1) % this.n;
+      while (this.counts[this.idx] == 0) {
+        this.idx = (this.idx + 1) % this.n;
+      }
+      while (!this.generators[this.idx].enable()) {
+        this.generators.splice(this.idx, 1);
+      }
+    }
+    if (event.key === "e") {
+      this.generators[this.idx].disable();
+      this.idx = (this.idx - 1 + this.n) % this.n;
+      while (this.counts[this.idx] == 0) {
+        this.idx = (this.idx - 1 + this.n) % this.n;
+      }
+      this.generators[this.idx].enable();
+    }
+  }
+
+  decrementCount() {
+    this.counts[this.idx]--;
+    //console.log(this.counts[this.idx]);
+    if (this.counts[this.idx] == 0) {
+      this.typesLeft--;
+      this.generators[this.idx].disable();
+      if (this.typesLeft == 0) {
+        if (this.points >= this.target) {
+          document.getElementById("end-screen").style.display = "block";
+          document.getElementById("end-message").innerHTML = "You win!";
+        } else {
+          document.getElementById("end-screen").style.display = "block";
+          document.getElementById("end-message").innerHTML = "You lose!";
         }
+        this.updateUI();
+        return;
+      }
+      while (this.counts[this.idx] == 0) {
+        this.idx = (this.idx + 1) % this.n;
+      }
+      this.generators[this.idx].enable();
+    }
+    this.updateUI();
+  }
+
+  updateUI() {
+    document.getElementById("b1-count").innerHTML = this.counts[0];
+    if (this.counts[0] < 1) {
+      document.getElementById("b1").classList.add("disabled");
+    }
+    document.getElementById("b2-count").innerHTML = this.counts[1];
+    if (this.counts[1] < 1) {
+      document.getElementById("b2").classList.add("disabled");
+    }
+    document.getElementById("b3-count").innerHTML = this.counts[2];
+    if (this.counts[2] < 1) {
+      document.getElementById("b3").classList.add("disabled");
     }
 
+    document.getElementById("light-count").innerHTML =
+      this.points + "/" + this.target;
+  }
 }
--- custom/Generator.js
+++ custom/Generator.js
@@ -1,214 +1,221 @@
 import {
-    Node,
-    Primitive,
-    Texture,
-    Sampler,
-    Material,
-    Model,
-    Transform
-} from '../engine/core.js';
+  Node,
+  Primitive,
+  Texture,
+  Sampler,
+  Material,
+  Model,
+  Transform,
+} from "../engine/core.js";
 
-import { gridCenter, gridSize, tileSize } from './gameParameters.js';
+import { gridCenter, gridSize, tileSize } from "./gameParameters.js";
 
 const generatorPattern = [
-    [
-        {x: 0, y: 0, type: "generator"},
-        {x: 1, y: 0, type: "yellow"},
-        {x: -1, y: 0, type: "yellow"},
-    ],
-    [
-        {x: 0, y: 0, type: "generator"},
-        {x: 0, y: 1, type: "yellow"},
-        {x: 0, y: 2, type: "yellow"},
-        {x: 0, y: -1, type: "yellow"},
-        {x: 0, y: -2, type: "yellow"},
-    ],
-    [
-        {x: 0, y: 0, type: "generator"},
-        {x: 0, y: 1, type: "generator"},
-        {x: 1, y: 0, type: "yellow"},
-        {x: 2, y: 0, type: "yellow"},
-        {x: -1, y: 0, type: "yellow"},
-        {x: -2, y: 0, type: "yellow"},
-        {x: 1, y: -1, type: "yellow"},
-        {x: -1, y: -1, type: "yellow"},
-        {x: 1, y: 1, type: "purple"},
-        {x: 2, y : 1, type: "purple"},
-        {x: -1, y: 1, type: "purple"},
-        {x: -2, y: 1, type: "purple"},
-        {x: 1, y: 2, type: "purple"},
-        {x: -1, y: 2, type: "purple"},
-
-    ],
+  [
+    { x: 0, y: 0, type: "generator" },
+    { x: 1, y: 0, type: "yellow" },
+    { x: -1, y: 0, type: "yellow" },
+  ],
+  [
+    { x: 0, y: 0, type: "generator" },
+    { x: 0, y: 1, type: "yellow" },
+    { x: 0, y: 2, type: "yellow" },
+    { x: 0, y: -1, type: "yellow" },
+    { x: 0, y: -2, type: "yellow" },
+  ],
+  [
+    { x: 0, y: 0, type: "generator" },
+    { x: 0, y: 1, type: "generator" },
+    { x: 1, y: 0, type: "yellow" },
+    { x: 2, y: 0, type: "yellow" },
+    { x: -1, y: 0, type: "yellow" },
+    { x: -2, y: 0, type: "yellow" },
+    { x: 1, y: -1, type: "yellow" },
+    { x: -1, y: -1, type: "yellow" },
+    { x: 1, y: 1, type: "purple" },
+    { x: 2, y: 1, type: "purple" },
+    { x: -1, y: 1, type: "purple" },
+    { x: -2, y: 1, type: "purple" },
+    { x: 1, y: 2, type: "purple" },
+    { x: -1, y: 2, type: "purple" },
+  ],
 ];
 
 const offsetToCamera = 1;
 
 export class Generator {
-    constructor(scene, node, tileArr, idx, gameManager) {
-        this.scene = scene;
-        this.node = node;
-        this.tileArr = tileArr;
-        this.idx = idx;
-        this.gameManager = gameManager;
-        this.baseTranslation = [offsetToCamera + gridCenter * tileSize, offsetToCamera, offsetToCamera + gridCenter * tileSize];
-        if (idx == 2) {
-            this.baseTranslation[2] += tileSize / 2;
-        }
-        this.transform = new Transform({
-            translation: [...this.baseTranslation],
-            scale: [tileSize, tileSize, tileSize],
-        });
-        
-        this.x = gridCenter;
-        this.y = gridCenter;
-        this.xNext = this.x;
-        this.yNext = this.y;
-        this.yMax = (idx == 2) ? gridSize - 1 : gridSize;
-        this.moveFlag = false;
-        this.status = "red";
+  constructor(scene, node, tileArr, idx, gameManager) {
+    this.scene = scene;
+    this.node = node;
+    this.tileArr = tileArr;
+    this.idx = idx;
+    this.gameManager = gameManager;
+    this.baseTranslation = [
+      offsetToCamera + gridCenter * tileSize,
+      offsetToCamera,
+      offsetToCamera + gridCenter * tileSize,
+    ];
+    if (idx == 2) {
+      this.baseTranslation[2] += tileSize / 2;
+    }
+    this.transform = new Transform({
+      translation: [...this.baseTranslation],
+      scale: [tileSize, tileSize, tileSize],
+    });
 
-        this.placeDownFlag = false;
+    this.x = gridCenter;
+    this.y = gridCenter;
+    this.xNext = this.x;
+    this.yNext = this.y;
+    this.yMax = idx == 2 ? gridSize - 1 : gridSize;
+    this.moveFlag = false;
+    this.status = "red";
 
-        this.node.removeComponentsOfType(Transform);
-        this.node.addComponent(this.transform);
+    this.placeDownFlag = false;
 
-        this.keyDownBind = this.handleKeyDown.bind(this);
+    this.node.removeComponentsOfType(Transform);
+    this.node.addComponent(this.transform);
+
+    this.keyDownBind = this.handleKeyDown.bind(this);
+  }
+
+  update(t, dt) {
+    if (this.moveFlag) {
+      this.moveFlag = false;
+
+      this.clearPattern();
+
+      this.x = this.xNext;
+      this.y = this.yNext;
+
+      this.setPattern();
     }
 
-    update(t, dt) {
-        if (this.moveFlag) {
-            this.moveFlag = false;
-            
-            this.clearPattern();
-
-            this.x = this.xNext;
-            this.y = this.yNext;
-
-            this.setPattern();
-        }
-
-        if (this.placeDownFlag) {
-            this.placeDownFlag = false;
-            if (this.status === "green") {
-                this.clearPattern();
-                this.placeDown();
-            }
-        }
-    }
-
-    clearPattern() {
-        for (const gp of generatorPattern[this.idx]) {
-            const x = this.x + gp.x;
-            const y = this.y + gp.y;
-            if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
-                this.tileArr[x][y].setTempStatus("none");
-            }
-        }
-    }
-
-    setPattern() {
-        this.status = "green";
-            for (const gp of generatorPattern[this.idx]) {
-                const x = this.x + gp.x;
-                const y = this.y + gp.y;
-
-                if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
-                    if (gp.type === "generator") {
-                        this.tileArr[x][y].setTempStatus("generator");
-                        if ( this.tileArr[x][y].permaStatus !== "yellow") {
-                            this.status = "red";
-                        }
-                    }
-                    else {
-                        const tempStatus = this.status == "green" ? (gp.type == "yellow" ? "green" : "darkGreen") : (gp.type == "yellow" ? "red" : "darkRed");
-                        this.tileArr[x][y].setTempStatus(tempStatus);
-                    }
-                }
-            }
-    }
-
-    placeDown() {
-        for (const gp of generatorPattern[this.idx]) {
-            const x = this.x + gp.x;
-            const y = this.y + gp.y;
-            if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
-                this.gameManager.points += this.tileArr[x][y].setPermaStatus(gp.type);
-            }
-        }
-        console.log(this.gameManager.points + "/" + this.gameManager.target);
-
-        const placedModel = new Node();
-        placedModel.addComponent(_.cloneDeep(this.node.getComponentOfType(Model)));
-        placedModel.addComponent(_.cloneDeep(this.transform));
-        this.scene.addChild(placedModel);
-
-        this.moveFlag = true;       
-        this.gameManager.decrementCount();
-    }
-
-    changeParent(newParent) {
-        this.node.remove();
-        newParent.addChild(this.node);
-    }
-
-    handleKeyDown(event) {
-        switch (event.key) {
-            case 'w':
-                if (this.yNext > 0) {
-                    this.yNext--;
-                    this.transform.translation[2] -= tileSize;
-                    this.moveFlag = true;
-                }
-                break;
-            case 'a':
-                if (this.xNext > 0) {
-                    this.xNext--;
-                    this.transform.translation[0] -= tileSize;
-                    this.moveFlag = true;
-                }
-                break;
-            case 's':
-                if (this.yNext < this.yMax - 1) {
-                    this.yNext++;
-                    this.transform.translation[2] += tileSize;
-                    this.moveFlag = true;
-                }
-                break;
-            case 'd':
-                if (this.xNext < gridSize - 1) {
-                    this.xNext++;
-                    this.transform.translation[0] += tileSize;
-                    this.moveFlag = true;
-                }
-                break;
-            case ' ': {
-                this.placeDownFlag = true;
-            }
-        }
-    }
-
-
-    enable() {
-        if (this.count == 0) {
-            return false;
-        }
-        this.node.enabled = true;
-        this.moveFlag = true;
-        this.xNext = gridCenter;
-        this.yNext = gridCenter;
-        this.transform.translation = [...this.baseTranslation];
-        window.addEventListener('keydown', this.keyDownBind);
-        //console.log("Generator enabled: " + this.idx);
-        return true;
-    }
-
-    disable() {
-        this.node.enabled = false;
-        this.moveFlag = false;
+    if (this.placeDownFlag) {
+      this.placeDownFlag = false;
+      if (this.status === "green") {
         this.clearPattern();
-        window.removeEventListener('keydown', this.keyDownBind);
-        //console.log("Generator disabled: " + this.idx);
+        this.placeDown();
+      }
     }
+  }
 
+  clearPattern() {
+    for (const gp of generatorPattern[this.idx]) {
+      const x = this.x + gp.x;
+      const y = this.y + gp.y;
+      if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
+        this.tileArr[x][y].setTempStatus("none");
+      }
+    }
+  }
+
+  setPattern() {
+    this.status = "green";
+    for (const gp of generatorPattern[this.idx]) {
+      const x = this.x + gp.x;
+      const y = this.y + gp.y;
+
+      if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
+        if (gp.type === "generator") {
+          this.tileArr[x][y].setTempStatus("generator");
+          if (this.tileArr[x][y].permaStatus !== "yellow") {
+            this.status = "red";
+          }
+        } else {
+          const tempStatus =
+            this.status == "green"
+              ? gp.type == "yellow"
+                ? "green"
+                : "darkGreen"
+              : gp.type == "yellow"
+              ? "red"
+              : "darkRed";
+          this.tileArr[x][y].setTempStatus(tempStatus);
+        }
+      }
+    }
+  }
+
+  placeDown() {
+    for (const gp of generatorPattern[this.idx]) {
+      const x = this.x + gp.x;
+      const y = this.y + gp.y;
+      if (x >= 0 && x < gridSize && y >= 0 && y < gridSize) {
+        this.gameManager.points += this.tileArr[x][y].setPermaStatus(gp.type);
+      }
+    }
+    //console.log(this.gameManager.points + "/" + this.gameManager.target);
+
+    const placedModel = new Node();
+    placedModel.addComponent(_.cloneDeep(this.node.getComponentOfType(Model)));
+    placedModel.addComponent(_.cloneDeep(this.transform));
+    this.scene.addChild(placedModel);
+
+    this.moveFlag = true;
+    this.gameManager.decrementCount();
+  }
+
+  changeParent(newParent) {
+    this.node.remove();
+    newParent.addChild(this.node);
+  }
+
+  handleKeyDown(event) {
+    switch (event.key) {
+      case "w":
+        if (this.yNext > 0) {
+          this.yNext--;
+          this.transform.translation[2] -= tileSize;
+          this.moveFlag = true;
+        }
+        break;
+      case "a":
+        if (this.xNext > 0) {
+          this.xNext--;
+          this.transform.translation[0] -= tileSize;
+          this.moveFlag = true;
+        }
+        break;
+      case "s":
+        if (this.yNext < this.yMax - 1) {
+          this.yNext++;
+          this.transform.translation[2] += tileSize;
+          this.moveFlag = true;
+        }
+        break;
+      case "d":
+        if (this.xNext < gridSize - 1) {
+          this.xNext++;
+          this.transform.translation[0] += tileSize;
+          this.moveFlag = true;
+        }
+        break;
+      case " ": {
+        this.placeDownFlag = true;
+      }
+    }
+  }
+
+  enable() {
+    if (this.count == 0) {
+      return false;
+    }
+    this.node.enabled = true;
+    this.moveFlag = true;
+    this.xNext = gridCenter;
+    this.yNext = gridCenter;
+    this.transform.translation = [...this.baseTranslation];
+    window.addEventListener("keydown", this.keyDownBind);
+    //console.log("Generator enabled: " + this.idx);
+    return true;
+  }
+
+  disable() {
+    this.node.enabled = false;
+    this.moveFlag = false;
+    this.clearPattern();
+    window.removeEventListener("keydown", this.keyDownBind);
+    //console.log("Generator disabled: " + this.idx);
+  }
 }
